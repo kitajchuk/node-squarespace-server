06c1c8114079ced696ab4b18cdfa4e6edb88e817
Brandon Kitajchuk <kitajchuk@gmail.com>
2014-11-18 00:38:21 -0800

    Removing testing code from squarespace-render.

--- squarespace-render.js
+++ squarespace-render.js
@@ -9,28 +9,6 @@ var jsonTemplate = require( "./lib/jsontemplate" ),
         more_predicates: require( "./lib/predicates" ),
         undefined_str: ""
     },
-    issueIfBlocks = [
-        "categories",
-        "categoryFilter"
-    ],
-
-
-/******************************************************************************
- * @Private
-*******************************************************************************/
-
-replaceIssueIfBlocks = function ( render ) {
-    var match;
-
-    for ( var i = issueIfBlocks.length; i--; ) {
-        while ( match = render.match( new RegExp( "{\\.if\s" + issueIfBlocks[ i ] + "}" ) ) ) {
-            render = render.replace( match[ 0 ], "{.section " + issueIfBlocks[ i ] + "}" );
-            render = render.replace( new RegExp( "{\\.repeated section " + issueIfBlocks[ i ] + "}" ), "{.repeated section @}" );
-        }
-    }
-
-    return render;
-},
 
 
 /******************************************************************************
@@ -47,11 +25,6 @@ replaceIssueIfBlocks = function ( render ) {
  *
  */
 renderJsonTemplate = function ( render, data ) {
-    // @temporary fix
-    // @issues: {.if categories}...{.end} seems to blow up unanimously
-    // @issues: {.if categoryFilter}...{.end} seems to blow up unanimously
-    //render = replaceIssueIfBlocks( render );
-
     render = jsonTemplate.Template( render, jsontOptions );
     render = render.expand( data );
 
